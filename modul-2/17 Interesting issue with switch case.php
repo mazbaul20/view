682316c6634810e6e3ea5854488d1b6e66468642
MD Mazbaul Islam <mazbaul20@gmail.com>
2023-02-01 11:58:15 +0600

    view

---
+++ modul-2/17 Interesting issue with switch case.php
@@ -0,0 +1,37 @@
+<?php
+    //GPA and grade point using switch statement
+    $mark = 85;
+    switch($mark){
+        case($mark>100):
+            echo"you are wrong!";
+            break;
+        case($mark>80):
+            echo"Grade A+, GPA 4.0";
+            break;
+        case($mark>75):
+            echo"Grade A, GPA 3.75";
+            break;
+        case($mark>70):
+            echo"Grade A-, GPA 3.5";
+            break;
+        case($mark>65):
+            echo"Grade B+, GPA 3.25";
+            break;
+        case($mark>60):
+            echo"Grade A+, GPA 3.0";
+            break;
+        case($mark>55):
+            echo"Grade B-, GPA 2.75";
+            break;
+        case($mark>50):
+            echo"Grade C+, GPA 2.50";
+            break;
+        case($mark>45):
+            echo"Grade C, GPA 2.25";
+            break;
+        case($mark>40):
+            echo"Grade D, GPA 2.00";
+            break;
+        default:
+            echo"Failled";
+    }
