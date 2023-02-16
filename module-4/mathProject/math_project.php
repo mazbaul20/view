c1888bad060b2cf69cb45d44a687caeefef0fc37
MD Mazbaul Islam <mazbaul20@gmail.com>
2023-02-17 01:53:27 +0600

    update project

--- module-4/mathProject/math_project.php
+++ module-4/mathProject/math_project.php
@@ -5,62 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <style>
-    .block{
-        width:400px;
-        height:400px;
-        background-color: #f9f9fa;
-        margin:0 auto;
-        position: relative;
-        font-family: sans-serif;
-        border-radius: 5px;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    }
-    .block h2{
-      text-align:center;
-      padding-top:10px;
-    }
-    .block table{
-        padding-left: 20px;
-    }
-    .showOutput{
-      padding-top:10px;
-      padding-left:28px;
-
-    }
-    .date{
-      position: absolute;
-      bottom: 0px;
-      padding:20px;
-      font-family: sans-serif;
-    }
-
-    .button1 {
-      background-color:#2596be;
-      padding: 7px 19px;
-      text-align: center;
-      display: inline-block;
-      font-size: 16px;
-      margin: 4px 2px;
-      cursor: pointer;
-      color:white;
-      border:none;
-      border-radius: 2px;
-      /* transition-duration: 0.4s; */
-    }
-
-    .button1:hover {
-      border: 1px solid #4CAF50;
-      color:black;
-      background-color: white;
-    }
-    input[type=text] {
-    width: 100%;
-    padding: 8px 17px;
-    margin: 8px 0;
-    box-sizing: border-box;
-  }
-  </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
@@ -89,19 +34,11 @@
       </form>
       <div class="showOutput">
         <?php
-//          if(isset($_POST['submit'])){
-//            if(!empty($_POST['num'])){
-//              $num = $_POST['num'];
-//              mathProject($num);
-//            }else{
-//              echo "<span style='color:red'><b>Number is Required!</b></span>";
-//            }
-//          }
             if(isset($_POST['submit'])){
                 if(empty($_POST['num'])){
-                    echo "<span style='color:red'><b>Number is Required!</b></span>";
+                    echo "<span style='color:#ef6262'><b>Number is Required!</b></span>";
                 }elseif(!is_numeric($_POST['num'])){
-                    echo "<span style='color:red'><b>Number is Required!</b></span>";
+                    echo "<span style='color:#ec5353'><b>String Not valid!</b></span>";
                 }else{
                     $num = $_POST['num'];
                     mathProject($num);
@@ -116,6 +53,5 @@
         ?>
       </div>
     </div>
-    
 </body>
 </html>
