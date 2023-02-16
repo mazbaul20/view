67a2de86dd32e5a61b12444aae824df333d2c003
MD Mazbaul Islam <mazbaul20@gmail.com>
2023-02-16 13:57:00 +0600

    update project

--- index.php
+++ index.php
@@ -1,47 +1,64 @@
-<?php
-    //GPA and grade point using switch statement
-   /*  $mark = 80;
-    switch($mark){
-        case($mark>100):
-            echo"you are wrong!";
-            break;
-        case($mark>=80):
-            echo"Grade A+, GPA 4.0";
-            break;
-        case($mark>=75):
-            echo"Grade A, GPA 3.75";
-            break;
-        case($mark>=70):
-            echo"Grade A-, GPA 3.5";
-            break;
-        case($mark>=65):
-            echo"Grade B+, GPA 3.25";
-            break;
-        case($mark>=60):
-            echo"Grade A+, GPA 3.0";
-            break;
-        case($mark>=55):
-            echo"Grade B-, GPA 2.75";
-            break;
-        case($mark>=50):
-            echo"Grade C+, GPA 2.50";
-            break;
-        case($mark>=45):
-            echo"Grade C, GPA 2.25";
-            break;
-        case($mark>=40):
-            echo"Grade D, GPA 2.00";
-            break;
-        default:
-            echo"Failled";
-    }
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <style>
+        body{
+            margin:0px;
+            border:0px;
+            padding:0px;
+            outline:0;
+        }
+        .pgBlock{
+            margin:0px auto;
+            width:80%;
+            height:80%;
+        }
 
- function Series(){
-    $result=0;
-    for($i=1;$i<=100;$i++){
-        $result+=$i;
-    }
-    return "1+2+3....100 = ".$result;
- }
- echo Series();
+    </style>
+</head>
+<body>
+    
+    <?php
+            function mathProject(int $num){
+                for ($i = 1; $i <= 10; $i++) {
+                    echo "{$i} x {$num} = ".$i*$num."<br/>";
+                }
+            }
+    ?>
+    <div class="pgBlock">
+        <h2>Simple Math Project</h2>
+        <form action="" method="POST" class="form">
+            <table class="table">
+                <tr>
+                    <td>Input</td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="reset">
+                        <input type="submit" name="submit" value="Submit">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+
+        <?php
+            if($_SERVER['REQUEST_METHOD']=="POST"){
+                if(empty($_POST['name'])){
+                    $errname = "<span style='color:red;'>Name is Required</span>";
+                }else{
+                    $num = $_POST['name'];
+                    $result = mathProject($num);
+                }
+                
+            }
+        ?>
+</body>
+</html>
